ff4f8075f75f1e7bab0dc042e04d91b752bde982
muvbed <muvbed@gmail.com>
2021-03-12 17:22:40 +0300

    added profile index page; added InsuredVehicle component

---
+++ components/Profile/InsuredVehicle.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="insuredVehicle">
+    <div class="insuredVehicle__link">
+      <img src="~/assets/images/icons/arrow-right-green-big.svg" alt="arrow">
+    </div>
+    <div class="insuredVehicle__vehicle">
+      {{ vehicleData.vehicle }}
+    </div>
+    <div class="insuredVehicle__number">
+      {{ vehicleData.number }}
+    </div>
+    <div class="insuredVehicle__drivers">
+      {{ vehicleData.drivers }}
+    </div>
+    <div class="insuredVehicle__details">
+      <div class="insuredVehicle__detailsInner">
+        <div class="insuredVehicle__detailsBlock">
+          <div class="insuredVehicle__detailsTitle">
+            Страховка
+          </div>
+          <div class="insuredVehicle__detailsText" v-html="vehicleData.insurance" />
+        </div>
+        <div class="insuredVehicle__detailsBlock">
+          <div class="insuredVehicle__detailsTitle">
+            Технический осмотр
+          </div>
+          <div class="insuredVehicle__detailsText" v-html="vehicleData.inspection" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InsuredVehicle',
+  props: {
+    vehicleData: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.insuredVehicle {
+    position: relative;
+    padding: 28px 53px 31px 43px;
+    background: #FFFFFF;
+    border-radius: 5px;
+    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.25), 4px 4px 40px rgba(0, 0, 0, 0.08);
+    &__link {
+        position: absolute;
+        top: 26px;
+        right: 14px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 34px;
+        height: 34px;
+        background-color: rgba(3, 134, 97, 0.06);
+        border-radius: 6px;
+        cursor: pointer;
+    }
+    &__vehicle {
+        margin-bottom: 8px;
+        font-size: 20px;
+        font-weight: bold;
+        letter-spacing: -0.02em;
+        color: #333333;
+    }
+    &__number {
+        margin-bottom: 6px;
+        font-size: 18px;
+        letter-spacing: -0.02em;
+        color: #828282;
+    }
+    &__drivers {
+        letter-spacing: -0.01em;
+        color: #BDBDBD;
+    }
+    &__details {
+        margin-top: 10px;
+        padding-top: 16px;
+        border-top: 1px solid #F2F2F2;
+    }
+    &__detailsInner {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+    }
+    &__detailsBlock {
+        max-width: 50%;
+        &:first-child {
+            margin-right: 30px;
+        }
+    }
+    &__detailsTitle {
+        margin-bottom: 5px;
+        text-transform: uppercase;
+        font-size: 12px;
+        letter-spacing: 1px;
+        color: #BDBDBD;
+    }
+    &__detailsText {
+        line-height: 23px;
+        letter-spacing: -0.01em;
+        color: #333333;
+    }
+}
+@media (max-width: 1023px) {
+    .insuredVehicle {
+        &__detailsInner {
+          flex-direction: column;
+        }
+        &__detailsBlock {
+          max-width: 100%;
+          &:first-child {
+            margin-right: 0;
+            margin-bottom: 16px;
+          }
+        }
+    }
+}
+@media (max-width: 1023px) {
+    .insuredVehicle {
+        padding: 15px 20px 18px;
+        &__link {
+            top: 15px;
+            width: 30px;
+            height: 30px;
+        }
+        &__vehicle {
+            margin-bottom: 0;
+            font-size: 18px;
+        }
+        &__number {
+            margin-bottom: 0;
+            font-size: 16px;
+        }
+        &__details {
+            display: none;
+        }
+    }
+}
+</style>
---
+++ pages/profile/index.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="profile">
+    <div class="profile__wrapper wrapper">
+      <h1 class="profile__title">
+        Личный кабинет
+      </h1>
+      <section class="profile__section">
+        <div class="profile__sectionHead">
+          <h2 class="profile__sectionTitle">
+            Машины и полисы
+          </h2>
+          <div class="profile__sectionAdd">
+            <span>+</span> Добавить
+          </div>
+        </div>
+        <div class="profile__sectionBody">
+          <InsuredVehicle
+            v-for="item in vehicles"
+            :key="item.id"
+            :vehicleData="item"
+          />
+        </div>
+      </section>
+      <section class="profile__section">
+        <div class="profile__sectionHead">
+          <h2 class="profile__sectionTitle">
+            Персоны
+          </h2>
+          <div class="profile__sectionAdd">
+            <span>+</span> Добавить
+          </div>
+        </div>
+        <div class="profile__sectionBody">
+
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import InsuredVehicle from '~/components/Profile/InsuredVehicle'
+export default {
+  components: {
+    InsuredVehicle
+  },
+  data () {
+    return {
+      vehicles: [
+        {
+          vehicle: 'Toyota Land Cruiser',
+          number: 'A 121 AA 163',
+          drivers: '1 водитель',
+          insurance: 'до 20 декабря 2020 (ещё 3 месяца)',
+          inspection: 'Нужен ТО для следующей страовки'
+        },
+        {
+          vehicle: 'Yamaha R3',
+          number: '5558 АВ 63',
+          drivers: '2 водителя',
+          insurance: 'до 20 сентября 2020 <span class="color-yellow">(осталось 5 дней)</span>',
+          inspection: 'ТО до 12 ноября 2021'
+        },
+        {
+          vehicle: 'Honda CBR 600',
+          number: 'А 121 АА  163',
+          drivers: '1 водитель',
+          insurance: '<span class="color-red">Истекла</span>',
+          inspection: 'ТО до 1 сентября 2021'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.profile {
+    &__wrapper {
+        padding-top: 50px;
+    }
+    &__title {
+      margin-bottom: 72px;
+      text-align: center;
+      letter-spacing: -0.02em;
+      color: #333333;
+    }
+    &__section {
+      margin-bottom: 75px;
+    }
+    &__sectionHead {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 40px;
+    }
+    &__sectionAdd {
+        display: flex;
+        align-items: center;
+        padding-left: 16px;
+        font-size: 18px;
+        font-weight: bold;
+        letter-spacing: -0.02em;
+        color: #088DED;
+        cursor: pointer;
+        span {
+            margin-right: 12px;
+            font-size: 26px;
+        }
+    }
+    &__sectionBody {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        .insuredVehicle {
+            width: calc(50% - 12px);
+            margin-bottom: 24px;
+            &:last-child {
+              margin-bottom: 0;
+            }
+        }
+    }
+}
+@media (max-width: 767px) {
+    .profile {
+        &__wrapper {
+            padding-top: 110px;
+        }
+        &__title {
+            margin-bottom: 28px;
+            font-size: 26px;
+        }
+        &__Section {
+          margin-bottom: 35px;
+        }
+        &__sectionHead {
+            margin-bottom: 16px;
+        }
+        &__sectionTitle {
+            font-size: 18px;
+        }
+        &__sectionAdd {
+            font-size: 16px;
+            span {
+                margin-right: 12px;
+                font-size: 20px;
+            }
+        }
+        &__sectionBody {
+            .insuredVehicle {
+                width: 100%;
+                margin-bottom: 12px;
+            }
+        }
+    }
+}
+</style>
